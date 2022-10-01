40648173a63dd04916ea09114251692a30b77682
bmargerison <bmargerison@outlook.com>
2022-10-01 16:51:14 +0100

    Add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>
-      <h1>Running Pace Calculator</h1>
-    </div>
+    <header className="p-3 bg-dark text-white">
+      <div className="container">
+        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+            <img src={require('../img/logo.svg').default} alt="logo" width={40} height={40} />
+        </div>
+      </div>
+    </header>
   )
 }
 
