5ad7a0892000f4935fb4ddf87bf401d2556a732f
bmargerison <bmargerison@outlook.com>
2022-10-01 11:21:24 +0100

    Add background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import background from './img/background.png'
 import Header from './components/Header'
+import Home from './components/Home'
 
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <div style={{ backgroundImage: `url(${background})`}}>
+      <Home />
+      </div>
+    </div>
   );
 }
 
