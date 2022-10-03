35e6f4da3f5db8d587f37a2b229591f1ae4b4935
bmargerison <bmargerison@outlook.com>
2022-10-03 18:30:49 +0100

    Header styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,11 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="p-3 bg-dark text-white">
+    <header className="d-flex flex-wrap justify-content-center py-3 bg-dark text-white">
       <div className="container">
-        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <img src={require('../img/logo.svg').default} alt="logo" width={40} height={40} />
+        <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto">
+          <img className="bi me-3" src={require('../img/logo.svg').default} alt="logo" width={40} height={40} />
+          <span className="fs-2">Running Pace Calculator</span>
         </div>
       </div>
     </header>
