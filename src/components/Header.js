bbb60fac754c6b389c71d59d0f88b6a9a35def5a
bmargerison <bmargerison@outlook.com>
2022-10-04 18:52:53 +0100

    Race time box styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import './Header.css'
 function Header() {
   return (
     <header className="d-flex flex-wrap justify-content-center py-3 bg-dark text-white">
-      <div className="container">
+      <div className="container mb-20">
         <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto">
           <img className="bi me-3" src={require('../img/logo.svg').default} alt="logo" width={40} height={40} />
           <span className="fs-2 title">Running Pace Calculator</span>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,25 @@
 import React from 'react'
-import './Header.css'
+import './Home.css'
 
-function Header() {
+function Home() {
   return (
     <div className="container min-vh-100">
-
+      <div className="container p-5 time-boxes">
+        <div className="row">
+          <div className="col box m-3">
+          </div>
+          <div className="col box m-3">
+          </div>
+        </div>
+        <div className="row">
+          <div className="col box m-3">
+          </div>
+          <div className="col box m-3">
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Header
+export default Home
