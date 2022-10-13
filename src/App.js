99ec21d8a6e6781217994f81e2ccfcf1a284a277
bmargerison <bmargerison@outlook.com>
2022-10-13 15:56:53 +0100

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header'
 import Home from './components/Home'
+import Results from './components/Results'
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header />
       <div className="background" >
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/results" element={<Results/>} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
