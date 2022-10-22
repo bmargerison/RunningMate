6c426275f62b385b56e3741a7682651af462eb9c
bmargerison <bmargerison@outlook.com>
2022-10-22 12:01:58 +0100

    getVdot function

--- src/data/Calculator.js
+++ src/data/Calculator.js
@@ -2,7 +2,12 @@ import { VO2MAX } from './Vo2MaxTable'
 import { DISTANCES } from './RaceDistancesEnum'
 
 const getVdot = (time) => {
-
+  const timeInSeconds = time.hours * 60 * 60 + time.minutes * 60 + time.seconds
+  const timesArray = VO2MAX.map(time => time[DISTANCES[5]])
+  const nearestTime = timesArray.reduce((prev, curr) => 
+    Math.abs(curr - timeInSeconds) < Math.abs(prev - timeInSeconds) ? curr : prev
+  );
+  return VO2MAX[timesArray.findIndex(x => x === nearestTime)].vdot
 }
 
 export { getVdot }
