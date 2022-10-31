bcd73c8d496b4240aad37ea50048c76d851adeae
bmargerison <bmargerison@outlook.com>
2022-10-31 10:44:31 +0000

    Add vdot box and styling to results page

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,8 +4,17 @@ import './Results.css'
 function Results() {
   
   return (
-    <div>
-      Results
+    <div className="container p-4">
+        <div className="row">
+          <div className="container box vdot-box">
+            <div className="row">
+              <h4>Your VDOT is:</h4>
+            </div>
+            <div className="row text-center">
+              <h1>30</h1>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
