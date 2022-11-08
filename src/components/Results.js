f1831615ceccd09eeea60800d7d4162c88669946
bmargerison <bmargerison@outlook.com>
2022-11-08 12:30:21 +0000

    Change predicted race time logic to iterator

--- src/components/Results.js
+++ src/components/Results.js
@@ -25,38 +25,21 @@ function Results() {
               <h4>Your predicted race times are:</h4>
             </div>
             <div className="row px-3 py-1">
-              <div className="col">
-                <div className="row">
-                  {DISTANCES[5]}
+            {
+              Object.keys(location.state.times.racePaces).map(function(distance, time) {
+                return (
+                  <div className="col">
+                  <div className="row">
+                    {distance}
+  
+                  </div>
+                  <div className="row">
+                    {location.state.times.racePaces[distance]}
+                  </div>
                 </div>
-                <div className="row">
-                  1:30
-                </div>
-              </div>
-              <div className="col">
-                <div className="row">
-                  {DISTANCES[10]}
-                </div>
-                <div className="row">
-                  1:30
-                </div>
-              </div>
-              <div className="col">
-                <div className="row">
-                  {DISTANCES.Half}
-                </div>
-                <div className="row">
-                  1:30
-                </div>
-              </div>
-              <div className="col">
-                <div className="row">
-                  {DISTANCES.Marathon}
-                </div>
-                <div className="row">
-                  1:30
-                </div>
-              </div>
+                )
+              })
+            }
             </div>
           </div>
         </div>
