865e815c55907d1947d42ab20041e6a9551bc37e
bmargerison <bmargerison@outlook.com>
2022-11-28 16:00:21 +0000

    Extract race times to separate component

---
+++ src/components/RaceTimes.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom';
+import './Results.css'
+import * as calcs from '../data/Calculator'
+
+function RaceTimes() {
+  const location = useLocation();
+  
+  return (
+    <div className="container">
+      <div className="row p-1">
+        <h4>Your predicted race times are:</h4>
+      </div>
+      <div className="row px-3 py-1">
+      {
+        Object.keys(location.state.times.racePaces).map(function(distance, i) {
+          return (
+            <div className="col px-4" key={i}>
+              <div className="row">
+                {distance}
+              </div>
+              <div className="row">
+                {calcs.getTime(location.state.times.racePaces[distance])}
+              </div>
+          </div>
+          )
+        })
+      }
+      </div>
+    </div>
+  )
+}
+
+export default RaceTimes
--- src/components/Results.js
+++ src/components/Results.js
@@ -1,47 +1,26 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
 import './Results.css'
-import * as calcs from '../data/Calculator'
+import RaceTimes from './RaceTimes'
 
 function Results() {
-  const location = useLocation();
-  console.log(location.state.times)
   
   return (
     <div className="container p-4">
-        <div className="row mt-3">
-          <div className="container box vdot-box">
-            <div className="row p-1">
-              <h4>Your VDOT is:</h4>
-            </div>
-            <div className="row p-2 text-center">
-              <h1>30</h1>
-            </div>
+      <div className="row mt-3">
+        <div className="container box vdot-box">
+          <div className="row p-1">
+            <h4>Your VDOT is:</h4>
+          </div>
+          <div className="row p-2 text-center">
+            <h1>30</h1>
           </div>
         </div>
-        <div className="row mt-4">
-          <div className="container box">
-            <div className="row p-1">
-              <h4>Your predicted race times are:</h4>
-            </div>
-            <div className="row px-3 py-1">
-            {
-              Object.keys(location.state.times.racePaces).map(function(distance, i) {
-                return (
-                  <div className="col" key={i}>
-                    <div className="row">
-                      {distance}
-                    </div>
-                    <div className="row">
-                      {calcs.getTime(location.state.times.racePaces[distance])}
-                    </div>
-                </div>
-                )
-              })
-            }
-            </div>
-          </div>
+      </div>
+      <div className="row mt-4">
+        <div className="container box">
+          <RaceTimes />
         </div>
+      </div>
     </div>
   )
 }
