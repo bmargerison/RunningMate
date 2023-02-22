c226bf2252022b2c0063bf23b7212c79f94f8ef8
bmargerison <bmargerison@outlook.com>
2023-02-22 13:26:27 +0000

    Add link to homepage on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import { NavLink } from "react-router-dom";
+import './Header.css';
 
 function Header() {
   return (
     <header className="d-flex flex-wrap justify-content-center py-3 bg-dark text-white">
       <div className="container mb-20">
-        <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto">
-          <img className="bi me-3" src={require('../img/logo.svg').default} alt="logo" width={40} height={40} />
-          <span className="fs-2 title">RunningMate</span>
-        </div>
+        <NavLink to="/" style={{ textDecoration: 'none' }}>
+          <div className="d-flex align-items-center mb-3 mb-md-0 me-md-auto">
+            <img className="bi me-3" src={require('../img/logo.svg').default} alt="logo" width={40} height={40} />
+            <span className="fs-2 title">RunningMate</span>
+          </div>
+        </NavLink>
       </div>
     </header>
   )
-}
+};
 
-export default Header
+export default Header;
